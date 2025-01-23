2a67bdaa12271ad9ef3d53a8d89114e135db33f3
andl2008 <andl2008@hotmail.com>
2025-01-23 12:21:10 +0000

    Primeiro execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const express = require ("express")
+const express = require("express")
 
 const app = express()
+
+
+app.get('/', (require, repost) => {
+    repost.send ("Olá mundo!")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
