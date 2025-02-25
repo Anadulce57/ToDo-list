086f96f16b8f1372b7e2477e815689cc48e61d56
Ana Dulce <andl2008@hotmail.com>
2025-02-25 09:06:00 -0300

    Exibição das tarefas na página home concluída (mais ou menos)

--- index.js
+++ index.js
@@ -49,10 +49,9 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
-
+    
+        resposta.render('home', {tarefas});
     })
-
-    resposta.render('home');
 })
 
 const conexao = mysql.createConnection({
