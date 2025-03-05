08b18fe3813e2e0cd6c7d02b8939c2e54cc5014f
Ana Dulce <andl2008@hotmail.com>
2025-03-05 08:45:12 -0300

    Agora sim ocorre a exibição das tarefas

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ app.get('/', (requisicao, resposta) => {
         const tarefas = dados.map((dado) => {
             return{
                 id: dado.id,
-                descricao: dado.descricao,
+                descricao: dado.descrical,
                 completa: dado.completa === 0 ? false : true
             }
         })
     
-        resposta.render('home', {tarefas});
+        resposta.render('home', { tarefas });
     })
 })
 
